9c3965954747158bba6601403faf0323a1165ae2
Haziq Ali <60395311+theHaziqali@users.noreply.github.com>
2023-12-22 19:15:07 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '12.4ED78@bd',
-  database: 'students_db',
+  database: 'patients_db',
 });
 
 db.connect((err) => {
